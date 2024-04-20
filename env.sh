e938db914d24898efc78bbad56ab2dec35566205
Ishank Agrawal <ishank@mit.edu>
2024-04-20 17:47:37 -0400

    update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,20 @@
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+#!/bin/bash
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+os_name=$(uname -s)
+
+# Check if the OS is macOS
+if [ "$os_name" == "Darwin" ]; then
+    echo "macOS detected."
+    export ANDROID_HOME=$HOME/Library/Android/sdk
+    export PATH=$PATH:$ANDROID_HOME/emulator
+    export PATH=$PATH:$ANDROID_HOME/platform-tools
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+# Check if the OS is Linux
+elif [ "$os_name" == "Linux" ]; then
+    echo "Linux detected."
+
+# Handle other operating systems
+else
+    echo "Unsupported operating system: $os_name"
+fi
