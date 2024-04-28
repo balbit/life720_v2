6f112b95c93b059e1a5e3a3eeef029fb5947f4aa
Elliot Liu <elliotliu123@gmail.com>
2024-04-28 12:59:26 -0400

    Modified config files
    Co-authored-by: Ishank Agrawal <mathletema@users.noreply.github.com>

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,23 @@
+const path = require('path');
 module.exports = {
   root: true,
   extends: '@react-native',
+  parset: '@babel/eslint-parser',
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@utils': './src/utils',
+        },
+      },
+    },
+  },
 };
--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
