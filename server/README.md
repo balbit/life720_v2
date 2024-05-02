0567ae762b4d22cd54df3bc23dce949a338d07a3
Elliot Liu <elliotliu123@gmail.com>
2024-05-02 18:50:04 -0400

    made swagger docs accessible

---
+++ server/README.md
@@ -0,0 +1,20 @@
+# Server README
+
+## Getting Started
+
+1. Install the dependencies by running the following command:
+```
+npm install
+```
+
+2. Start the server
+```
+npm start
+```
+
+## API
+
+Access the API docs at 
+```
+http://localhost:3000/api-docs
+```
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/basicRoutes';
 import database from './database/Database';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
+
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +17,7 @@ app.use('/api', router);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
 
 const run = async () => {
@@ -21,98 +26,8 @@ const run = async () => {
   const ishank_id = "user-7ce7c2e4";
   const elliot_id = "user-d1f1e35d";
 
-  console.log(`ishank_id: ${ishank_id}`);
-  console.log(`elliot_id: ${elliot_id}`);
-
-  let ishank_friends = await database.get_friends(ishank_id);
-  let elliot_friends = await database.get_friends(elliot_id);
-  console.log(ishank_friends)
-  console.log(elliot_friends)
-
-  await database.make_friends(ishank_id, elliot_id);
-
-  ishank_friends = await database.get_friends(ishank_id);
-  elliot_friends = await database.get_friends(elliot_id);
-  console.log(ishank_friends)
-  console.log(elliot_friends)
-
-  const current_ts: number = new Date().getTime();
-
-  await database.add_location(ishank_id, current_ts, {latitude: 10, longitude: 10});
-  await database.add_location(elliot_id, current_ts, {latitude: 10, longitude: 10});
-  await new Promise((resolve) => { setTimeout(resolve, 2000); });
-  await database.add_location(ishank_id, current_ts, {latitude: 30, longitude: 40});
-
-  console.log("Getting current location")
-
-  const location = await database.get_current_location(ishank_id);
-  console.log(location)
-
-  const location_2 = await database.get_friends_current_location(elliot_id);
-  console.log(location_2)
   console.log("Done!")
 
 }
 
 run();
-
-
-// const express = require('express');
-// const app = express();
-// const userRoutes = require('./routes/userRoutes');
-
-// // Middleware
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/users', userRoutes);
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// import express from 'express';
-// const app = express();
-// app.use(express.json())
-
-// const port = 3000;
-
-// let log : Array<Array<any>> = [];
-
-// function createTable(tableData: Array<Array<string | number>>) : string {
-//     var res = "";
-//     tableData.forEach(function(rowData) {
-//         let row_res = ""
-//       rowData.forEach(function(cellData: string | number) {
-//         row_res += `${cellData} `;
-//       });
-//       res += row_res + "<br />";
-//     });
-//     return res;
-//   } 
-
-// app.get('/', (req, res) => {
-//     res.send(`<!DOCTYPE html>
-// <html>
-// <head>
-//     <title>Life 720 Server</title>
-// </head>
-// <body>
-//     <h1>Life 720</h1>
-//     ${createTable(log)}
-// </body>
-// </html>`)
-// })
-
-// app.post('/logger/', (req, res) => {
-//     console.log("got connection!")
-//     log.push([req.body.id, req.body.timestamp, req.body.lat, req.body.long])
-//     res.send()
-//     console.log(log)
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
