a4b9bd76fb96d06e6a90aab1a9c47af63b1be96f
Elliot Liu <elliotliu123@gmail.com>
2024-05-05 15:06:20 -0400

    get friend locations

--- client/src/services/ServerService.ts
+++ client/src/services/ServerService.ts
@@ -1,5 +1,5 @@
 import {LocationInfo, Location} from '@/../../common/types/types'
-import {SendLocationRequest} from '@/../../common/types/requests'
+import {SendLocationRequest, GetFriendLocationsRequest, GetFriendLocationsResponse} from '@/../../common/types/requests'
 import { getUserID } from './UserService';
 import {Config} from 'react-native-config'
 
@@ -45,5 +45,35 @@ export async function sendLocation(locationInfo: LocationInfo): Promise<void> {
         console.log(`Successfully sent location: ${location.latitude}, ${location.longitude}`)
     } catch (error) {
         console.error(error);
+    }
+}
+
+export async function getFriendLocations(): Promise<Array<LocationInfo>> {
+
+    const userID = getUserID();
+    console.log(`Getting friend locations for user: ${userID}`);
+
+    const getFriendLocationsRequest: GetFriendLocationsRequest = {
+        userID: userID
+    }
+
+    const requestInit: RequestInit = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    try {
+        const response = await fetch(serverAPILink(`getFriendLocation/${userID}`), requestInit);
+        if (!response.ok) {
+            console.error(`Failed to get friend location: ${response.statusText}`);
+            throw new Error(`Failed to get friend location: ${response.statusText}`);
+        }
+        const getFriendLocationsResponse: GetFriendLocationsResponse = await response.json();
+        return getFriendLocationsResponse.locations;
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
 }
